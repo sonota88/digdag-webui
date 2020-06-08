2fd58ee1515c0f6406ad82111d2194d8b9d4a8c3
sonota88 <yosiot8753@gmail.com>
2020-06-09 07:13:38 +0900

    __wip 2020-06-09 07:13:38 Remove patches

--- app/app.rb
+++ app/app.rb
@@ -238,8 +238,6 @@ module Digdag
   end
 end
 
-require "digdag_utils/task"
-
 module DigdagUtils
   class Workflow
   end
@@ -248,73 +246,9 @@ module DigdagUtils
   end
 
   class Attempt
-    def initialize(
-          id:               nil,
-          done:             nil,
-          success:          nil,
-          cancel_requested: nil
-        )
-      @id               = id
-      @done             = done
-      @success          = success
-      @cancel_requested = cancel_requested
-    end
-    
-    def self.from_api_response(data)
-      new(
-        id:               data["id"],
-        done:             data["done"],
-        success:          data["success"],
-        cancel_requested: data["cancelRequested"]
-      )
-    end
-
-    def to_plain
-      {
-        id:               @id,
-        done:             @done,
-        success:          @success,
-        cancel_requested: @cancel_requested,
-      }
-    end
   end
 
   class Task
-    attr_reader :full_name
-    attr_reader :parent_id, :upstreams
-    attr_reader :is_group
-
-    def initialize(
-      id: nil,
-      name: nil,
-      state: nil,
-      full_name:        nil,
-      cancel_requested: nil,
-      parent_id:        nil,
-      upstreams:        nil,
-      is_group:         nil
-    )
-      @id = id
-      @name = name
-      @state = state
-      @full_name        = full_name
-      @cancel_requested = cancel_requested
-      @parent_id        = parent_id
-      @upstreams        = upstreams
-      @is_group         = is_group
-    end
-
-    def self.from_api_response(data)
-      new(
-        id:               data["id"],
-        full_name:        data["fullName"],
-        state:            data["state"],
-        cancel_requested: data["cancelRequested"],
-        parent_id:        data["parentId"],
-        upstreams:        data["upstreams"],
-        is_group:         data["isGroup"]
-      )
-    end
   end
 end
 
