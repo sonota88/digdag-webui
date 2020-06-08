78e4cd199390d1eba0fe81e3b27c80065a9f3476
sonota88 <yosiot8753@gmail.com>
2020-06-09 08:35:22 +0900

    __wip 2020-06-09 08:35:22 Remove feature: show graph (session)

--- app/public/js/session/page_show.js
+++ app/public/js/session/page_show.js
@@ -28,15 +28,6 @@ class View {
 
       , h("hr")
 
-      , h("button"
-        , { onclick: ()=>{ __p.onclick_showGraph(); } }
-        , "show graph"
-        )
-      , h("hr")
-      , h("img", { id: "graph_img"
-                   // , width: "100%"
-                 })
-      , h("hr")
       , Attempts.render(state.attempts)
       )
     );
@@ -75,8 +66,6 @@ class Page {
 
       this.render();
 
-      // this.showGraph();
-
     }, (errors)=>{
       __g.unguard();
       __g.printApiErrors(errors);
@@ -88,36 +77,6 @@ class Page {
     $("#main")
       .empty()
       .append(View.render(this.state));
-  }
-
-  showGraph(){
-    puts("onclick_showGraph");
-    __g.guard();
-
-    const attemptId =
-      Math.max(
-        ...this.state.attempts.map(att => _parseInt(att.id))
-      );
-
-    __g.api_v2(
-      "get",
-      `/api/${this.env}/attempts/${attemptId}/graph`,
-      {},
-      (result)=>{
-        __g.unguard();
-        puts(result);
-  
-        $("#graph_img").attr("src", result.path);
-  
-      }, (errors)=>{
-        __g.unguard();
-        __g.printApiErrors(errors);
-        alert("Check console.");
-      });
-  }
-
-  onclick_showGraph(){
-    this.showGraph();
   }
 }
 
