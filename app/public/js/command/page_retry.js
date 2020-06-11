4ea1e1d9ded6d781e213b1788a45079f212e598c
sonota88 <yosiot8753@gmail.com>
2020-06-11 21:07:31 +0900

    Implement View class (retry)

--- app/public/js/command/page_retry.js
+++ app/public/js/command/page_retry.js
@@ -1,5 +1,37 @@
 class View {
-
+  static render(state){
+    return TreeBuilder.build(h =>
+      h("div", {}
+       , h("textarea", { id: "input_box", style: {
+               width: "80%"
+             , height: "10rem"
+             }
+           }
+         ,
+`${__p.attemptId} \\
+  --latest-revision \\
+  --resume \\
+  -e {endpoint}
+`
+         )
+       , h("br")
+       , h("button", {
+             onclick: ()=>{ __p.onclick_run(); }
+           }
+         , "Run"
+         )
+       , h("pre", {
+         style: { background: "#eeeeee" }
+       }
+       ,
+`digdag retry {attemptId} ...
+--latest-revision | --keep-revision (exclusive)
+--resume | --resume-from {task_name} | --all (exclusive)
+`
+        )
+      )
+    );
+  }
 }
 
 class Page {
