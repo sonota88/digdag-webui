aa386d7d71f26791c6059337f53c36b72e9e599b
sonota88 <yosiot8753@gmail.com>
2020-06-14 06:35:21 +0900

    Add created_at, finished_at (DigdagUtils::Attempt)

--- app/app.rb
+++ app/app.rb
@@ -266,6 +266,58 @@ module DigdagUtils
   end
 
   class Attempt
+    def initialize(
+      id: nil,
+      done:             nil,
+      success:          nil,
+      cancel_requested: nil,
+      created_at:       nil,
+      finished_at:      nil,
+      session:          nil
+    )
+      @id = id
+      @done             = done
+      @success          = success
+      @cancel_requested = cancel_requested
+      @created_at       = created_at
+      @finished_at      = finished_at
+      @session          = session
+    end
+
+    def self.from_api_response(data)
+      wf = Workflow.new(
+        id:   data["workflow"]["id"],
+        name: data["workflow"]["name"]
+      )
+
+      sess = Session.new(
+        id: data["sessionId"],
+        time: Time.parse(data["sessionTime"]),
+        workflow: wf
+      )
+
+      new(
+        id:               data["id"],
+        done:             data["done"],
+        success:          data["success"],
+        cancel_requested: data["cancelRequested"],
+        created_at:       DigdagUtils.from_plain_time(data["createdAt"]),
+        finished_at:      DigdagUtils.from_plain_time(data["finishedAt"]),
+        session:          sess
+      )
+    end
+
+    def to_plain
+      {
+        id:               @id,
+        done:             @done,
+        success:          @success,
+        cancel_requested: @cancel_requested,
+        created_at:       DigdagUtils.to_plain_time(@created_at),
+        finished_at:      DigdagUtils.to_plain_time(@finished_at),
+        session:          @session.to_plain,
+      }
+    end
   end
 
   class Task
