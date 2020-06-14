e4801b64df13a44bea6d1acc1583ed54932dc03f
sonota88 <yosiot8753@gmail.com>
2020-06-14 11:01:22 +0900

    Adjust bottom pane layout

--- app/public/js/common_component.js
+++ app/public/js/common_component.js
@@ -266,9 +266,9 @@ class BottomPane {
             id: opts.id
           , style: {
               position: "fixed"
-            , left: "0"
+            , left: "30vw"
             , bottom: "0"
-            , width: "100vw"
+            , width: "70vw"
             , height: "50vh"
             , background: "rgba(127,127,127, 0.2)"
             , "z-index": "10"
@@ -277,9 +277,9 @@ class BottomPane {
         , h("div", {
             style: {
                 position: "fixed"
-              , left: "0vw"
+              , left: "30vw"
               , bottom: "0"
-              , width: "100vw"
+              , width: "70vw"
               , height: "50vh"
               , background: "#fff"
               , "box-shadow": "0rem 0rem 2rem rgba(0,0,0, 0.2)"
@@ -298,7 +298,7 @@ class BottomPane {
               )
             )
           , h("div", { style: {
-                  height: "calc(80vh - 3rem)"
+                  height: "calc(50vh - 3rem)"
                 , padding: "1rem"
                 }
               }
