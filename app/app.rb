a73a9052147f4ebe292af35fe7e5e20f80b40adf
sonota88 <yosiot8753@gmail.com>
2020-07-20 08:51:01 +0900

    __wip 2020-07-20 08:51:01 Pass task updated_at, started_at, retry_at

--- app/app.rb
+++ app/app.rb
@@ -253,6 +253,61 @@ module DigdagUtils
   end
 
   class Task
+    attr_reader :updated_at, :started_at, :retry_at
+
+    def initialize(
+      id: nil,
+      state: nil,
+      full_name:        nil,
+      cancel_requested: nil,
+      parent_id:        nil,
+      upstreams:        nil,
+      is_group:         nil,
+      updated_at: nil,
+      started_at: nil,
+      retry_at:   nil
+    )
+      @id = id
+      @state = state
+      @full_name        = full_name
+      @cancel_requested = cancel_requested
+      @parent_id        = parent_id
+      @upstreams        = upstreams
+      @is_group         = is_group
+      @updated_at = updated_at
+      @started_at = started_at
+      @retry_at   = retry_at
+    end
+
+    def self.from_api_response(data)
+      new(
+        id:               data["id"],
+        full_name:        data["fullName"],
+        state:            data["state"],
+        cancel_requested: data["cancelRequested"],
+        parent_id:        data["parentId"],
+        upstreams:        data["upstreams"],
+        is_group:         data["isGroup"],
+        updated_at: DigdagUtils.from_plain_time(data["updatedAt"]),
+        started_at: DigdagUtils.from_plain_time(data["startedAt"]),
+        retry_at:   DigdagUtils.from_plain_time(data["retryAt"  ]),
+      )
+    end
+
+    def to_plain
+      {
+        id:               @id,
+        state:            @state,
+        full_name:        @full_name,
+        cancel_requested: @cancel_requested,
+        parent_id:        @parent_id,
+        upstreams:        @upstreams,
+        is_group:         @is_group,
+        updated_at: DigdagUtils.to_plain_time(@updated_at),
+        started_at: DigdagUtils.to_plain_time(@started_at),
+        retry_at:   DigdagUtils.to_plain_time(@retry_at),
+      }
+    end
   end
 end
 
